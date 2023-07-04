0fefdf2ae8c68b7a1ba4d9fefbafc290b5249ffe
MBareiro <58647378+MBareiro@users.noreply.github.com>
2023-07-03 23:26:49 -0300

    Actualizacion de codigo

--- BackEnd/controladores.py
+++ BackEnd/controladores.py
@@ -2,66 +2,63 @@ from flask import Flask, jsonify, request
 from app import app, ma
 from modelos import *
 
-class ProductoSchema(ma.Schema):
+class UsuarioSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'nombre', 'precio', 'stock', 'imagen')
+        fields = ('id', 'nombre', 'apellido', 'direccion')
 
-# El objeto producto_schema es para traer un producto
-producto_schema = ProductoSchema()
-# El objeto productos_schema es para traer multiples registros de producto
-productos_schema = ProductoSchema(many=True)
+# El objeto usuario_schema es para traer un usuario
+usuario_schema = UsuarioSchema()
+# El objeto usuarios_schema es para traer multiples registros de usuario
+usuarios_schema = UsuarioSchema(many=True)
 # crea los endpoint o rutas (json)
 
-@app.route('/productos', methods=['GET'])
-def get_Productos():
+@app.route('/usuarios', methods=['GET'])
+def get_Usuarios():
     # el metodo query.all() lo hereda de db.Model
-    all_productos = Producto.query.all()
+    all_usuarios = Usuario.query.all()
     # el metodo dump() lo hereda de ma.schema y
-    result = productos_schema.dump(all_productos)
+    result = usuarios_schema.dump(all_usuarios)
     # trae todos los registros de la tabla
     # retorna un JSON de todos los registros de la tabla
     return jsonify(result)
 
-@app.route('/productos/<id>', methods=['GET'])
-def get_producto(id):
-    producto = Producto.query.get(id)
-    # retorna el JSON de un producto recibido como parametro
-    return producto_schema.jsonify(producto)
+@app.route('/usuarios/<id>', methods=['GET'])
+def get_usuario(id):
+    usuario = Usuario.query.get(id)
+    # retorna el JSON de un usuario recibido como parametro
+    return usuario_schema.jsonify(usuario)
 
 
-@app.route('/productos/<id>', methods=['DELETE'])
-def delete_producto(id):
-    producto = Producto.query.get(id)
-    db.session.delete(producto)
+@app.route('/usuarios/<id>', methods=['DELETE'])
+def delete_usuario(id):
+    usuario = Usuario.query.get(id)
+    db.session.delete(usuario)
     db.session.commit()
     # me devuelve un json con el registro eliminado
-    return producto_schema.jsonify(producto)
+    return usuario_schema.jsonify(usuario)
 
-@app.route('/productos', methods=['POST'])  # crea ruta o endpoint
-def create_producto():
+@app.route('/usuarios', methods=['POST'])  # crea ruta o endpoint
+def create_usuario():
     # print(request.json)  # request.json contiene el json que envio el cliente
     nombre = request.json['nombre']
-    precio = request.json['precio']
-    stock = request.json['stock']
-    imagen = request.json['imagen']
-    new_producto = Producto(nombre, precio, stock, imagen)
-    db.session.add(new_producto)
+    apellido = request.json['apellido']
+    direccion = request.json['direccion']
+    new_usuario = Usuario(nombre, apellido, direccion)
+    db.session.add(new_usuario)
     db.session.commit()
-    return producto_schema.jsonify(new_producto)
+    return usuario_schema.jsonify(new_usuario)
 
-@app.route('/productos/<id>', methods=['PUT'])
-def update_producto(id):
-    producto = Producto.query.get(id)
+@app.route('/usuarios/<id>', methods=['PUT'])
+def update_usuario(id):
+    usuario = Usuario.query.get(id)
 
     nombre = request.json['nombre']
-    precio = request.json['precio']
-    stock = request.json['stock']
-    imagen = request.json['imagen']
+    apellido = request.json['apellido']
+    direccion = request.json['direccion']
 
-    producto.nombre = nombre
-    producto.precio = precio
-    producto.stock = stock
-    producto.imagen = imagen
+    usuario.nombre = nombre
+    usuario.apellido = apellido
+    usuario.direccion = direccion
 
     db.session.commit()
-    return producto_schema.jsonify(producto)
+    return usuario_schema.jsonify(usuario)
--- BackEnd/modelos.py
+++ BackEnd/modelos.py
@@ -1,20 +1,18 @@
 from app import db, app
 
 # defino la tabla
-class Producto(db.Model):   # la clase Producto hereda de db.Model
+class Usuario(db.Model):   # la clase Usuario hereda de db.Model
     # define los campos de la tabla
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100))
-    precio = db.Column(db.Integer)
-    stock = db.Column(db.Integer)
-    imagen = db.Column(db.String(400))
+    apellido = db.Column(db.String(100))
+    direccion = db.Column(db.String(100))
 
-    def __init__(self, nombre, precio, stock, imagen):  # crea el  constructor de la clase
+    def __init__(self, nombre, apellido, direccion):  # crea el  constructor de la clase
         # no hace falta el id porque lo crea sola mysql por ser auto_incremento
         self.nombre = nombre
-        self.precio = precio
-        self.stock = stock
-        self.imagen = imagen
+        self.apellido = apellido
+        self.direccion = direccion
 
 with app.app_context():
     db.create_all()  # aqui crea todas las tablas
